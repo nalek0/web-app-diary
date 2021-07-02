5cc45a2b16e65463c0d294433acb9dbe1853502e
Nalek <belash.aleksandr777@gmail.com>
2021-07-02 22:36:35 +0300

    Added auto resize to textarea

--- web/static/js/pages/set-day.js
+++ web/static/js/pages/set-day.js
@@ -10,7 +10,19 @@ Vue.component('set-day', {
 					<div><input class="input page_block" type="text" placeholder="Год" name="year"></div>
 					<div><input class="input page_block" type="text" placeholder="Месяц" name="month"></div>
 					<div><input class="input page_block" type="text" placeholder="День" name="day"></div>
-					<div><textarea class="input page_block" name="text" style="height: 100px; resize: none;" placeholder="Текст"></textarea></div>
+					<div>
+						<textarea 
+							class="input page_block" 
+							name="text" 
+							style="min-height: 100px; resize: none;" 
+							placeholder="Текст"
+							@change = 	"resize()"
+							@cut = 		"delayedResize()"
+							@paste = 	"delayedResize()"
+							@drop = 	"delayedResize()"
+							@keydown = 	"delayedResize()">
+						</textarea>
+					</div>
 					<div class="buttons">
 						<div class="button update-btn" @click="set_today()">Сегодня</div>
 						<div class="button submit-btn" @click="send_data()">Отправить</div>
@@ -40,6 +52,23 @@ Vue.component('set-day', {
 			document.querySelector("input[name='year']").value 	= today.getFullYear();
 			document.querySelector("input[name='month']").value = today.getMonth() + 1;
 			document.querySelector("input[name='day']").value 	= today.getDate();
+		},
+		resize() {
+			// this.textarea.style.height = 'auto';
+			// this.textarea.style.height = this.textarea.scrollHeight+'px';
+			if (this.textarea.offsetHeight < this.textarea.scrollHeight) {
+				$(this.textarea).animate({
+					height: this.textarea.scrollHeight + "px"
+				}, 500);
+			}
+		},
+		delayedResize() {
+			window.setTimeout(this.resize, 0);
+		}
+	},
+	computed: {
+		textarea() {
+			return document.querySelector('textarea');
 		}
 	}
 });
